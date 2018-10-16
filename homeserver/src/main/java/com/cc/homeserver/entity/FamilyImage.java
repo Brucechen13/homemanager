a07de9ced28f764c14de638917a1662b953e9478
Brucechen13 <chenchi_94@163.com>
2018-10-16 22:41:42 +0800

    add

--- homeserver/src/main/java/com/cc/homeserver/entity/FamilyImage.java
+++ homeserver/src/main/java/com/cc/homeserver/entity/FamilyImage.java
@@ -1,6 +1,8 @@
 package com.cc.homeserver.entity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import lombok.experimental.Accessors;
 
 import javax.persistence.*;
@@ -8,10 +10,15 @@ import javax.validation.constraints.NotBlank;
 import java.sql.Timestamp;
 
 @Data
+@EqualsAndHashCode(exclude={"sender", "group"})
+@ToString(exclude={"sender", "group"})
 @Accessors(chain = true)
 @Entity
 @Table(name = "family_image")
 public class FamilyImage {
+
+    @Id@GeneratedValue
+    private int id;
 
     @Column(nullable=false)
     private String picUrl;
